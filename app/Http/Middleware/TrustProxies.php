dde991abaf1496fde9f839ebd780228e2afd0666
Pilal <rafihilalzahir@gmail.com>
2026-01-30 14:17:35 +0700

    port tunneling https

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    protected $proxies = '*';
+
+    protected $headers =
+        \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR |
+        \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST |
+        \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PORT |
+        \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO;
+}
